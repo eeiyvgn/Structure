4807339de4f3135868d0aadfa88585fbd0725ab3
Evgeniy <e.koskov@bk.ru>
2025-01-29 16:24:25 +0500

    complete first task

--- main.cpp
+++ main.cpp
@@ -1,4 +1,28 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <cstdint>
+
+#pragma pack(push, 1)
+
+struct FileRecrodHeaderFormat
+{
+    uint32_t TextString;
+    uint16_t OffsetOfTheFileRecord;
+    uint16_t SizeOfTheFileRecord;
+    uint64_t SequenceNumber;
+    uint16_t SequantialSectorNumber;
+    uint16_t NumberOfFileReferences;
+    uint16_t OffsetOfTheFirstAttribute;
+    uint16_t UsageAndDirectoryFlags;
+    uint32_t ActualSizeOfTheFileEntry;
+    uint32_t AllocatedFileRecordSize;
+    uint64_t FileReference;
+    uint32_t ID;
+    uint32_t FileEntryNumber;
+};
+
+#pragma pack(pop)
 
 using namespace std;
 
