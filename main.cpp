807ddab90efd399933ac1c3bec62677b456b4762
Evgeniy <e.koskov@bk.ru>
2025-01-29 18:06:39 +0500

    complete third task

--- main.cpp
+++ main.cpp
@@ -3,9 +3,11 @@
 #include <vector>
 #include <cstdint>
 
+using namespace std;
+
 #pragma pack(push, 1)
 
-struct FileRecrodHeaderFormat
+struct FileRecordHeaderFormat
 {
     uint32_t TextString;
     uint16_t OffsetOfTheFileRecord;
@@ -22,9 +24,22 @@ struct FileRecrodHeaderFormat
     uint32_t FileEntryNumber;
 };
 
-#pragma pack(pop)
+struct AttributeHeaderFormat
+{
+    uint32_t AttributeType;
+    uint32_t AttributeLength;
+    uint8_t NonResidentAttributeFlag;
+    uint8_t AttributeNameLength;
+    uint16_t AttributeNameOffset;
+    uint16_t AttributePackingFlag;
+    uint16_t AttributeID;
+    uint32_t SizeOfAttributeContentInBytes;
+    uint16_t OffsetOfAttributeContent;
+    uint8_t IndexingFlag;
+    uint8_t Filling;
+};
 
-using namespace std;
+#pragma pack(pop)
 
 int main()
 {
@@ -40,11 +55,39 @@ int main()
 
         dat_file.read(dat_data.data(), fileSize);
 
-        FileRecrodHeaderFormat* frhf = reinterpret_cast<FileRecrodHeaderFormat*>(&dat_data[0]);
+        FileRecordHeaderFormat* frhf = reinterpret_cast<FileRecordHeaderFormat*>(&dat_data[0]);
 
         cout << "Text string: " << hex << int(frhf->TextString) << endl;
         cout << "Offset of the first attribute: " << hex << int(frhf->OffsetOfTheFirstAttribute) << endl;
         cout << "File entry number: " << hex << int(frhf->FileEntryNumber) << endl;
+
+        size_t offset  = frhf->OffsetOfTheFirstAttribute;
+
+        while (offset + sizeof(AttributeHeaderFormat) < fileSize)
+        {
+            AttributeHeaderFormat* ahf = reinterpret_cast<AttributeHeaderFormat*>(&dat_data[offset]);
+
+            if (ahf->AttributeType != 0xFFFFFFFF)
+            {
+                cout << "Attribute name: ";
+                switch (ahf->AttributeType)
+                {
+                case 0x10: cout << "$STANDART_INFORMATION" << endl; break;
+                case 0x20: cout << "$ATTRIBUTE_LIST" << endl; break;
+                case 0x30: cout << "$FILE_NAME" << endl; break;
+                case 0x40: cout << "$OBJECT_ID" << endl; break;
+                case 0x50: cout << "$SECURITY_DESCRIPTOR" << endl; break;
+                case 0x80: cout << "$DATA" << endl; break;
+                case 0x90: cout << "$INDEX_ROOT" << endl; break;
+                case 0xA0: cout << "$INDEX_ALLOCATION" << endl; break;
+                case 0xB0: cout << "$BITMAP" << endl; break;
+                case 0x100: cout << "$LOGGED_UTILITY_STREAM" << endl; break;
+                }
+                cout << "Attribute type: "<< hex << ahf->AttributeType << endl;
+            }
+
+            offset += ahf->AttributeLength;
+        }
     }
 
     return 0;
